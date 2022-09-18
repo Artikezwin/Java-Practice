7bb9370581068e74c40790b1c948b1682e7ccb8f
Artemka <Inkognito@example.com>
2022-09-18 18:55:13 +0300

    GUI, сложение чисел

--- src/praktika4/GUI.java
+++ src/praktika4/GUI.java
@@ -2,10 +2,12 @@ package praktika4;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class GUI extends JFrame{
 
-    JTextField jta = new JTextField(3); //3 - сколько символов видно в текстовом поле
+    /*JTextField jta = new JTextField(3); //3 - сколько символов видно в текстовом поле
     Font fnt = new Font("Calibri", Font.BOLD, 20); //шрифт, жирный текст, размер
 
     public GUI(){
@@ -17,8 +19,39 @@ class GUI extends JFrame{
         jta.setFont(fnt);
 
         setVisible(true);
-    }
+    }*/
 
+    JTextField jta1 = new JTextField(10);
+    JTextField jta2 = new JTextField(10);
+
+    JButton button = new JButton(" Тыкай");
+
+    Font fnt = new Font("Times new roman", Font.BOLD, 18);
+
+    GUI() {
+        super("Example");
+        setLayout(new FlowLayout());
+        setSize(250, 150);
+        add(new JLabel("1st Number"));
+        add(jta1);
+        add(new JLabel("2nd Number"));
+        add(jta2);
+        add(button);
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed (ActionEvent ae){
+                try {
+                    double x1 = Double.parseDouble(jta1.getText().trim());
+                    double x2 = Double.parseDouble(jta2.getText().trim());
+
+                    JOptionPane.showMessageDialog(null, "Result =" + (x1 + x2), "Alert", JOptionPane.INFORMATION_MESSAGE);
+                }
+                catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Error in Numbers!", "alert", JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+        setVisible(true);
+    }
 
 }
 
